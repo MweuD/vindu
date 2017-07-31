8d77d36c94817c99cbd0dadc83f6e11ee2106366
Daniel <daniel@localhost.localdomain>
2017-07-31 16:28:44 +0300

    Changed

--- run.py
+++ run.py
@@ -2,6 +2,13 @@ from app import app
 import os
 
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
 if __name__ == '__main__':
 	# Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
