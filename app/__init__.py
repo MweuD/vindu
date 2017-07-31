b28bf7ae35ebf7fce2eb70365c34db1207b0ecba
Daniel <daniel@localhost.localdomain>
2017-07-31 16:48:19 +0300

    Changed

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,5 @@ from flask import Flask
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
 
-
-import app.views
-
 # Load the config file
-app.config.from_object('config')
+
