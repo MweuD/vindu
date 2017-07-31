85263c2ebf04ce4b91ec35c75ec9f08fa82afefc
Daniel <daniel@localhost.localdomain>
2017-07-31 15:43:05 +0300

    Changed

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
-from app import app
+from flask import Flask
 import os
 
 
+app = Flask(__name__)
+
 if __name__ == '__main__':
-	# Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host = '0.0.0.0',port=port, debug = True)
-    
+
+    if port == 5000:
+        app.debug = True
+
+    app.run(host='0.0.0.0', port=port)
